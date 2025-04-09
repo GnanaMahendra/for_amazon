7e175a5cee4d52986d23a496b4aca18ce774fa44
gnanamahendra28 <gnanamahendra28@gmail.com>
2025-04-09 16:18:22 +0530

    first commit

---
+++ merge_String_alternately.py
@@ -0,0 +1,50 @@
+'''
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.'''
+
+word1 = input()
+word2 = input()
+w1_len = len(word1)
+w2_len = len(word2)
+max_len = max(w1_len,w2_len)
+result_word = ""
+for i in range(max_len):
+    if i < w1_len:
+        result_word += word1[i]
+    if i <  w2_len:
+        result_word += word2[i] 
+print(result_word)
